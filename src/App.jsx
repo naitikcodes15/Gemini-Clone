92338d70e14bd12c37194b7c45c6a3ae241b346d
Naitik Yadav <naitik.yadav641@gmail.com>
2025-10-01 17:53:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./Components/Main/Main"
+import Sidebar from "./Components/Sidebar/sidebar"
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main/>
+    </>
+  )
+}
+
+export default App
